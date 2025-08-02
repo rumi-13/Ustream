b995a523757fe2a0b431b0df06c7960b1ee45f4c
Asgar Rashid <asgar.raza013@gmail.com>
2025-08-02 08:42:39 +0530

    disable recommendations

--- src/pages/VideoPage.jsx
+++ src/pages/VideoPage.jsx
@@ -1,5 +1,5 @@
 import VideoPlayer from "../components/VideoPlayer";
-import Recommendations from "../components/Recommendations";
+//import Recommendations from "../components/Recommendations";
 import { useParams } from "react-router-dom";
 
 export default function VideoPage() {
@@ -9,17 +9,17 @@ export default function VideoPage() {
   return (
     <div
       className="
-        bg-white text-gray-800 dark:bg-neutral-900 dark:text-gray-100
-        pt-[calc(var(--navbar-height,4rem)+1rem)]
-        px-4 sm:px-6 md:px-4 py-4
+        bg-white text-gray-800  dark:bg-neutral-900
+        pt-2
+       
         min-h-screen
       "
     >
       <div className="flex flex-row flex-nowrap gap-6 ">
         {/* Left Column: To Play Video and render comments */}
         <div
-          className="flex-1 min-w-0 h-[calc(100vh-4rem)] overflow-y-auto pr-2
-        bg-gray-50 dark:bg-neutral-800
+          className="flex-1 w-full h-[calc(100vh-4rem)] overflow-y-auto pr-2
+        bg-white dark:bg-neutral-900
             rounded-xl shadow-sm ring-1 ring-gray-200/60 dark:ring-neutral-700/50
          
         "
@@ -28,7 +28,7 @@ export default function VideoPage() {
         </div>
 
         {/* Rcommendations column */}
-        <Recommendations categoryId={categoryId} id={id} />
+        {/* <Recommendations categoryId={categoryId} id={id} /> */}
       </div>
     </div>
   );
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
  
  plugins: [react()],
- //base: process.env.VITE_BASE_PATH || "/ustream",
+ base: process.env.VITE_BASE_PATH || "/ustream",
 
 })
